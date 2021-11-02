68f7feb5242cfe586c1047f6baff5f2002981b9f
David Alvarez <davidalvarez_4@hotmail.com>
2021-11-02 19:37:05 -0300

    Primer Commit

---
+++ src/main/java/com/Eggfinal/EggfinalApplication.java
@@ -0,0 +1,13 @@
+package com.Eggfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggfinalApplication.class, args);
+	}
+
+}
