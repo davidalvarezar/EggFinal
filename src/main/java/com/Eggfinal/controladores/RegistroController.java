6752ebabe72fe4a13c8fcc03ca01860ec32c9828
Agustin Satlari <agusatlari@gmail.com>
2021-11-17 20:53:37 -0300

    controladores sin html

---
+++ src/main/java/com/Eggfinal/controladores/RegistroController.java
@@ -0,0 +1,17 @@
+package com.Eggfinal.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class RegistroController {
+	
+	@GetMapping("/registrate")
+	public String registrousuario(){
+		
+		return "nombre del html";
+	}
+
+}
